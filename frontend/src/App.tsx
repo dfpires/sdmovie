d9d9fa13c265def4d76829dda971a99accaf0df2
dfpires@gmail.com <dfpires@gmail.com>
2022-01-11 00:31:12 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
 import Navbar from "./components/Navbar";
+import Form from "pages/Form";
+import Listing from "pages/Listing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+          <Route path="/" element={<Listing/>}> </Route> 
+          <Route path="/form">
+             <Route path=":movieId" element={<Form/>}/> 
+          </Route> 
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 

04aece2712428c61b78816c56f1151e8a82c2269
dfpires@gmail.com <dfpires@gmail.com>
2022-01-15 13:47:25 -0300

    useState e useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,14 +2,20 @@ import axios from "axios";
 import MovieCard from "components/MovieCard";
 
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
 
 function Listing(){
 
-    axios.get(`${BASE_URL}/movies`)
+    const [pageNumber, setPageNumber] = useState(0)
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
             console.log(response.data)
         })
+    }, [])
+  
     return (
         <>
             <Pagination/>

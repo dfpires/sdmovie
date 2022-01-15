2bc5a0d9d2cbcf0c4ae0549f2d950422a41128ce
dfpires@gmail.com <dfpires@gmail.com>
2022-01-15 14:03:54 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,42 +3,54 @@ import MovieCard from "components/MovieCard";
 
 import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing(){
 
     const [pageNumber, setPageNumber] = useState(0)
 
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    })
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title `)
         .then(response => {
-            console.log(response.data)
+            const movies = response.data as MoviePage
+            setPage(movies)
         })
-    }, [])
+    }, [pageNumber])
   
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
     return (
         <>
             <Pagination/>
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <MovieCard/>
+                    
+                    {page.content.map(movie => (
+
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard movie={movie}/>
                     </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard/>
-                    </div>
+                    ))}
+                    
                 </div>
             </div>
             
